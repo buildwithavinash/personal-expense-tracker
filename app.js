1f924ee1f98ef542626ecf4eb301b38aff7d2402
Avinash <avinash.developer.26@gmail.com>
2026-02-13 14:17:55 +0530

    improved styling and added manifest and sw

--- app.js
+++ app.js
@@ -259,3 +259,10 @@ if (clearBtn) {
 }
 
 
+// service worker
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js")
+    .then(() => console.log("Service Worker Registered"))
+    .catch(err => console.log("SW failed", err));
+}
