33620eceda11cc9c25c10a4548caa903bd147b3c
Avinash <avinash.developer.26@gmail.com>
2026-02-13 20:27:39 +0530

    update

--- sw.js
+++ sw.js
@@ -1,22 +1,30 @@
 const CACHE_NAME = "finance-app-v2";
 
+// Use relative paths to ensure it works on GitHub Pages subfolders
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        // Cache hit - return response, otherwise fetch from network
+        return response || fetch(event.request);
+      })
   );
 });
